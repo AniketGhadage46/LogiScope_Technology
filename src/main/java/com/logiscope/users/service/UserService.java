461a930ab55d35bd75a70f948dfdc6b96f73b0f1
AniketGhadage46 <121543120+AniketGhadage46@users.noreply.github.com>
2025-04-02 16:27:28 +0530

    Update UserService.java

--- src/main/java/com/logiscope/users/service/UserService.java
+++ src/main/java/com/logiscope/users/service/UserService.java
@@ -1,9 +1,11 @@
 package com.logiscope.users.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.logiscope.users.exceptions.UserNotFoundException;
+import com.logiscope.users.model.ResponseObject;
 import com.logiscope.users.model.User;
 import com.logiscope.users.repo.UserRepository;
 
@@ -19,22 +21,29 @@ public class UserService {
     private UserRepository userRepository;
 
     // user create
-    public User createUser(User user) {
+    public ResponseEntity<ResponseObject<User>> createUser(User user) {
+        // Set the password expiry date (30 days from current date)
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date()); // Set current date
         calendar.add(Calendar.DAY_OF_YEAR, 30); // Add 30 days to the current date
         user.setPasswordExpiryDate(calendar.getTime()); // Set password expiry date
-
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        String message = "User created successfully!";
+        ResponseObject<User> responseObject = new ResponseObject<>(message, savedUser);
+        return ResponseEntity.ok(responseObject);
     }
     
     // Bulk create users
-    public List<User> createUsers(List<User> users) {
+    public ResponseEntity<ResponseObject<List<User>>> createUsers(List<User> users) {
         for (User user : users) {
-            user.setPasswordExpiryDate(getPasswordExpiryDate()); //pass 30 day expiry
+            user.setPasswordExpiryDate(getPasswordExpiryDate()); // Pass 30-day expiry
         }
-        return userRepository.saveAll(users);
+        List<User> savedUsers = userRepository.saveAll(users);
+        String message = "Bulk Users created successfully!";
+        ResponseObject<List<User>> responseObject = new ResponseObject<>(message, savedUsers);
+        return ResponseEntity.ok(responseObject);
     }
+
     
     // Helper method to calculate password expiry date
     private Date getPasswordExpiryDate() {
@@ -47,7 +56,9 @@ public class UserService {
 
     // search by email
     public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        return userOptional;
+
     }
 
     // Get all users
@@ -57,9 +68,15 @@ public class UserService {
     
     
     // Method to update a user
-    public User updateUser(User user) {
-        return userRepository.save(user);
+    public ResponseEntity<ResponseObject<User>> updateUser(User user) {
+        user.setPasswordExpiryDate(getPasswordExpiryDate());
+        User updatedUser = userRepository.save(user);
+        String message = "User updated successfully!";
+        ResponseObject<User> responseObject = new ResponseObject<>(message, updatedUser);
+        return ResponseEntity.ok(responseObject);
     }
+
+
 
     // Method to delete a user by email
     public void deleteUserByEmail(String email) {
